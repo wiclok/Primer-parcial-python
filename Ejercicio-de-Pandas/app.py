8c40f5081bc695917bf17443ab29fca0959ce9a8
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-05-03 11:30:24 -0300

    A simulation of purchase and stock update was carried out

--- Ejercicio-de-Pandas/app.py
+++ Ejercicio-de-Pandas/app.py
@@ -1,6 +1,6 @@
 productos = [
   {"nombre": "Camiseta", "precio": 20, "cantidad_disponible":100},
-  {"nombre": "Pantalón", "precio": 30, "cantidad_disponible": 80},
+  {"nombre": "Pantalon", "precio": 30, "cantidad_disponible": 80},
   {"nombre": "Zapatos", "precio": 50, "cantidad_disponible": 50},
   {"nombre": "Reloj", "precio": 100, "cantidad_disponible": 30},
   {"nombre": "Gorra", "precio": 15, "cantidad_disponible": 120},
@@ -27,12 +27,53 @@ def calcular_valor_de_inventario_por_producto(productos):
     producto.update({"valor_total" : value_total})
   return productos
 
+def simulacion_de_ventas(valor_total_por_productos):
+  final = 0
+  subtotal = []
+  print('Un cliente decide comprar 5 camisetas, 3 zapatos, 5 pantalones, 1 collar y 1 reloj: ')
+  for producto in valor_total_por_productos:
+    if producto['nombre'] == 'Camiseta':
+      subtotal.append(producto['precio'] * 5)
+      producto.update({"cantidad_disponible": producto["cantidad_disponible"] - 5})
+      producto.update({"valor_total" : producto["precio"] * producto["cantidad_disponible"]})
+      print(producto)
+    if producto['nombre'] == 'Zapatos':
+      subtotal.append(producto['precio'] * 3)
+      producto.update({"cantidad_disponible": producto["cantidad_disponible"] - 3})
+      producto.update({"valor_total" : producto["precio"] * producto["cantidad_disponible"]})
+      print(producto)
+    if producto['nombre'] == 'Pantalon':
+      subtotal.append(producto['precio'] * 5)
+      producto.update({"cantidad_disponible": producto["cantidad_disponible"] - 5})
+      producto.update({"valor_total" : producto["precio"] * producto["cantidad_disponible"]})
+      print(producto)
+    if producto['nombre'] == 'Collar':
+      subtotal.append(producto['precio'] * 1)
+      producto.update({"cantidad_disponible": producto["cantidad_disponible"] - 1})
+      producto.update({"valor_total" : producto["precio"] * producto["cantidad_disponible"]})
+      print(producto)
+    if producto['nombre'] == 'Reloj':
+      subtotal.append(producto['precio'] * 1)
+      producto.update({"cantidad_disponible": producto["cantidad_disponible"] - 1})
+      producto.update({"valor_total" : producto["precio"] * producto["cantidad_disponible"]})
+      print(producto)
+  print('')
+  print('El stock acutal es de: ')
+  print(productos)
+  for precio in subtotal:
+    final += precio
+  print('')
+  print('El precio final de la compra fue de: $', final)
+  
+
 def ejecutar_programa():
   
   valor_total_por_producto = calcular_valor_de_inventario_por_producto(productos)
+  print('')
   print('Valor total por producto agregado:')
   print(valor_total_por_producto)
-  
+  print('')
+  simulacion_de_ventas(valor_total_por_producto)
   
 if __name__ == '__main__':
     ejecutar_programa()
